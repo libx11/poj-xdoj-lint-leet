3f274f7bf22a2eac4e406848d987d57359452404
libx <libx@stu.xidian.edu.cn>
2017-09-16 00:05:17 +0800

    2.0
    添加java作业

---
+++ Java homework/src/sec/input.java
@@ -0,0 +1,18 @@
+package sec;
+import java.util.*;
+
+public class input
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("WTF");
+        String name = in.nextLine();
+
+        System.out.print("Wtf");
+        int age = in.nextInt();
+
+        System.out.println("HHHHH." + name);
+    }
+}
